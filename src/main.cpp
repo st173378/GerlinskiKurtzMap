e896d5f94216ea889fac5b1a3bceca07a7ef3fa4
st173378 <st173378@stud.uni-stuttgart.de>
2021-12-05 11:36:13 +0100

    cleaned

--- src/main.cpp
+++ src/main.cpp
@@ -20,61 +20,47 @@ tuple<int,int,double,double,int> parseNode(const string a){
 
 }
 int main() {
-
     time_t currentTime;
     time(&currentTime);
     int startTime = currentTime;
-    int nthreads = std::thread::hardware_concurrency();
-    cout << nthreads << endl;
-
-    ifstream inputStream("../resources/germany.txt");
-    cout << inputStream.good() << endl;
-    cout << inputStream.is_open() << endl;
+    int a, b, e, f, g;
+    int nodeNumber, vertexNumber;
+    double c, d;
     string line;
-    int nodeNumber;
-    int vertexNumber;
+
+    vector<tuple<int, int, double, double, int>> nodes;
+    vector<tuple<int, int, int, int, int>> vertices;
+
+    ifstream inputStream("germany.fmi");
+
+    // Skip header comment
     for (int i = 0; i < 5; ++i) {
         getline(inputStream,line);
     }
-    getline(inputStream,line);
-    istringstream iss(line);
-    iss >> nodeNumber;
-    vector<tuple<int,int,double,double,int>> nodes;
+    // Read nodeNumber
+    inputStream >> nodeNumber;
     nodes.reserve(nodeNumber);
 
-    getline(inputStream,line);
-    istringstream ass(line);
-    ass >> vertexNumber;
-    vector<tuple<int,int,double,double,int>> vertices;
+    // Read vertexNumber
+    inputStream >> vertexNumber;
     vertices.reserve(vertexNumber);
+
+    // Read nodes
     for (int i = 0; i < nodeNumber; ++i) {
-        getline(inputStream,line);
-        int a;
-        int b;
-        double c;
-        double d;
-        int e;
-        istringstream stringStream(line);
-        stringStream >> a >> b >> c >> d >> e;
-        nodes.push_back({a,b,c,d,e});
+        inputStream >> a >> b >> c >> d >> e;
+        nodes.push_back({a, b,c ,d, e});
     }
+
+    // Read vertex
     for (int i = 0; i < vertexNumber; ++i) {
-        getline(inputStream,line);
-        int a;
-        int b;
-        double c;
-        double d;
-        int e;
-        istringstream stringStream(line);
-        stringStream >> a >> b >> c >> d >> e;
-        vertices.push_back({a,b,c,d,e});
+        inputStream >> a >> b >> f >> g >> e;
+        vertices.push_back({a, b, f, g, e});
     }
+
     time(&currentTime);
     int timeElapsed = currentTime - startTime;
     cout<<"It took "<<timeElapsed<<" seconds to run the program"<<endl;
-
-
-
+    return 0;
 	return 0;
 }
 
